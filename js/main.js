06c3c31be5a356360bc5baf86cf8859bc416004d
nonecover <adam.jakomulski@gmail.com>
2017-10-19 15:22:31 +0100

    on click animation

--- js/main.js
+++ js/main.js
@@ -13,3 +13,15 @@ $(".left-nav-button").click(function (button) {
     $(".top-nav-button, .bottom-nav-button, .right-nav-button").removeClass('nav-button-hover nav-button').addClass('left-nav-button-click');
     $("main").addClass('left-nav-hide');
 });
+
+$(".top-nav-button").click(function (button) {
+    $(this).removeClass('nav-button-hover nav-button').addClass('top-nav-button-click');
+    $(".left-nav-button, .bottom-nav-button, .right-nav-button").removeClass('nav-button-hover nav-button').addClass('top-nav-button-click');
+    $("main").addClass('top-nav-hide');
+});
+
+$(".bottom-nav-button").click(function (button) {
+    $(this).removeClass('nav-button-hover nav-button').addClass('bottom-nav-button-click');
+    $(".top-nav-button, .left-nav-button, .right-nav-button").removeClass('nav-button-hover nav-button').addClass('bottom-nav-button-click');
+    $("main").addClass('bottom-nav-hide');
+});
